bccb9da2e40ccfa39d03bcdb323961c9ab09ed30
Scholar <scholar@paradigm.local>
2023-09-05 13:18:59 -0600

    created variables and a function

---
+++ variables.py
@@ -0,0 +1,14 @@
+my_Name = "Oliver Skeem"
+Best_Test_Score = 100
+was_My_Lunch_Tasty = True
+my_B_Day_Classes = (
+    "Kedge",
+    "Seminar",
+    "computer programming",
+    "math"
+)
+
+print(f"Hi, my name is {my_Name}. I just scored {Best_Test_Score} on my last test. My classes today are {my_B_Day_Classes.}")
+
+def add_two_numbers():
+    print(1+2)
