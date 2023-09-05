df3d971457dd437a374543778466ddb47e5f71db
Scholar <scholar@paradigm.local>
2023-09-05 12:35:17 -0600

    created variables and a function

---
+++ potato.py
@@ -0,0 +1,30 @@
+person = "Barrett"
+
+# for i in range(10):
+#    print(i)
+
+    list = ["one", "two", "three",]
+    dictionary = (
+        "name": "Barrett",
+        "age": 16
+    )
+
+
+def is_even_number(number):
+    if number % 2 == 0:
+        print("This is an even number!")
+    else:
+        print("This is an odd number")
+
+is_even_number(43)
+
+MAIN_CHARACTER = "Mario"
+
+main_character = "Luigi"
+
+myNameIsNot = "Oliver Skeem"
+
+my_name = "Bill"
+
+MyName = "Bob"
+
