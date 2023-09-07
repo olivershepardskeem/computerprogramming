b885c298784d00f3bac623db8a14807cd56a98b5
Scholar <scholar@paradigm.local>
2023-09-07 13:31:48 -0600

    intro to concepts

---
+++ Intro to Key Programming Concepts.py
@@ -0,0 +1,45 @@
+my_name = "Mr Hunt"
+
+def give_my_name():
+    a_name = "William"
+    print(a_name)
+
+    print(my_name)
+
+    user_input = input("What is your name? ")
+    print(f"nice to meet you, {user_input}!")
+
+# data types
+    my_string = "This is my string,"
+    my_integer = 1456
+    my_float = 3.0
+is_my_boolean = False
+
+    print(my_integer + my_float)
+
+my_decleration = ""
+my_initialization = "Hello world"
+
+add = 1 + 2
+subtract = 2 - 1
+divide = 2 / 1
+multiply = 1 * 3
+modulo = 2 % 1
+exponent = 2 ** 3
+
+print(modulo)
+
+statement = 4 > 2
+statement2 = 4 >= 2
+statement3 = 4 <= 2
+statement4 = 4 == 2
+statement5 = 4 != 2
+
+print(statement3)
+
+
+message = 4 > 2 and 33 < 43
+message2 = 4 < 2 or 33 < 43
+x = true
+
+print(not x)
