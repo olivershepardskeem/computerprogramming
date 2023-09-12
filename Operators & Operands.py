6cb4ee0a645daad1d0619ec88e85b37686c08fb4
Scholar <scholar@paradigm.local>
2023-09-12 13:01:40 -0600

    operators

--- Operators & Operands.py
+++ Operators & Operands.py
@@ -1,1 +1,30 @@
-7 % 2
+modulo = 2 % 1
+
+multiply = 1 * 3
+
+divide = 2 / 1
+
+divide_float = 2.5 / 2.3
+
+add = 1 + 2
+
+subtract = 2 - 1
+
+statement = 4 < 5
+
+statement2 = 4 > 2
+
+statement3 = 4 == 2
+
+statement4 = 4 >= 4
+
+statement5 = 4 <= 4
+
+statement6 = x = 4
+
+statement7 = 4 != 2
+
+2 == 2 and 3 == 2
+
+2 == 2 or 2 == 3
+not 2 == 3
