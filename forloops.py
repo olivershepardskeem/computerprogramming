5abf4fb643a61d9c3f5aa72e560d557bf745dff3
Scholar <scholar@paradigm.local>
2023-10-03 12:43:07 -0600

    for loops

--- forloops.py
+++ forloops.py
@@ -13,8 +13,81 @@
 # for i in flowers:
 #     print(i)
 
+# list1 = [5,10,15,20]
+# list2 = ['Tomatoes','Potatoes','Carrots','Cucumbers']
 
+# for i in list1:
+#     for j in list2:
+#         print(i, j)
 
+# numbers = [12,3,56,67,89,90]
+# sum = 0
 
+# for i in numbers:
+#     sum += i
+# print(sum)
 
+# vehicles = ['Car','Cycle','Bus','Tempo']
 
+# for i in vehicles:
+#     if i == "Bus":
+#         break
+#     else: print(i)
+
+# vehicles = ['Car', 'Cycle', 'Bus', 'Tempo']
+# for vehicle in vehicles:
+#    print(vehicle)
+#    if vehicle == 'Bus': break
+
+# vehicles = ['Car', 'Cycle', 'Bus', 'Tempo']
+# for vehicle in vehicles:
+#      if vehicle == 'Bus':
+#          continue
+# print(vehicle)
+
+# numbers = [12, 3, 56, 67, 89, 90]
+# count = 0
+# for num in numbers:
+#  count += 1 
+#  print(count)
+
+# numbers = [12, 3, 56, 67, 89, 90]
+# sum = 0
+# for num in numbers: sum += num
+# print(sum)
+
+# numbers = [2, 5, 6, 10, 15, 20, 25]
+# for num in numbers:
+#      if num % 5 == 0:
+#           print(num)
+
+# list1 = ['Mango', 'Banana', 'Orange']
+# list2 = []
+# for item in list1: list2.append(item) 
+# print(list2)
+
+# numbers = [1, 4, 50, 80, 12]
+# max_num = numbers[0]
+# for num in numbers:
+#      if num > max_num: max_num = num
+#      print("Maximum:", max_num)
+
+# numbers = [1, 4, 50, 80, 12]
+# min_num = numbers[0]
+# for num in numbers:
+#      if num < min_num: min_num = num
+#      print("Minimum:", min_num)
+
+# numbers = [1, 4, 50, 80, 12]
+# numbers.sort() 
+# print("Ascending Order:", numbers)
+
+# numbers = [1, 4, 50, 80, 12]
+# numbers.sort(reverse=True)
+# print("Descending Order:", numbers)
+
+# for i in range(3, 31, 3): print(i)
+
+# for i in range(5, 51, 5): print(i)
+
+# for i in range(10, 0, -1): print(i)
